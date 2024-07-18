5c4f79f1b06f44701745e256d671a2eab70f3897
John Oliver <1615532+johnoliver@users.noreply.github.com>
2024-07-18 13:57:19 +0100

    update

--- samples/java/learnDocs/LightsApp/src/main/java/LightsApp.java
+++ samples/java/learnDocs/LightsApp/src/main/java/LightsApp.java
@@ -1,90 +1,107 @@
 // Copyright (c) Microsoft. All rights reserved.
 
+import com.azure.ai.openai.OpenAIAsyncClient;
+import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.credential.AzureKeyCredential;
 import com.google.gson.Gson;
-// <Imports>
-import com.azure.ai.openai.*;
-import com.microsoft.semantickernel.*;
-import com.microsoft.semantickernel.aiservices.openai.chatcompletion.*;
-import com.microsoft.semantickernel.contextvariables.*;
-import com.microsoft.semantickernel.orchestration.*;
-import com.microsoft.semantickernel.plugin.*;
-import com.microsoft.semantickernel.services.chatcompletion.*;
-// <Imports>
+import com.microsoft.semantickernel.Kernel;
+import com.microsoft.semantickernel.aiservices.openai.chatcompletion.OpenAIChatCompletion;
+import com.microsoft.semantickernel.contextvariables.ContextVariableTypeConverter;
+import com.microsoft.semantickernel.contextvariables.ContextVariableTypes;
+import com.microsoft.semantickernel.orchestration.InvocationContext;
+import com.microsoft.semantickernel.orchestration.InvocationReturnMode;
+import com.microsoft.semantickernel.orchestration.ToolCallBehavior;
+import com.microsoft.semantickernel.plugin.KernelPlugin;
+import com.microsoft.semantickernel.plugin.KernelPluginFactory;
+import com.microsoft.semantickernel.services.chatcompletion.AuthorRole;
+import com.microsoft.semantickernel.services.chatcompletion.ChatCompletionService;
+import com.microsoft.semantickernel.services.chatcompletion.ChatHistory;
+import com.microsoft.semantickernel.services.chatcompletion.ChatMessageContent;
 import java.util.List;
 import java.util.Scanner;
 
 public class LightsApp {
 
-    private static final String AZURE_CLIENT_KEY = System.getenv("AZURE_CLIENT_KEY");
-    private static final String CLIENT_ENDPOINT = System.getenv("CLIENT_ENDPOINT");
-    private static final String MODEL_ID = System.getenv().getOrDefault("MODEL_ID", "gpt-4o");
+  private static final String AZURE_CLIENT_KEY = System.getenv("AZURE_CLIENT_KEY");
+  private static final String CLIENT_ENDPOINT = System.getenv("CLIENT_ENDPOINT");
+  private static final String MODEL_ID = System.getenv().getOrDefault("MODEL_ID", "gpt-4o");
 
-    public static void main(String[] args) throws Exception {
+  public static void main(String[] args) throws Exception {
 
-        // <LightAppExample>
-        OpenAIAsyncClient client = new OpenAIClientBuilder()
-            .credential(new AzureKeyCredential(AZURE_CLIENT_KEY))
-            .endpoint(CLIENT_ENDPOINT)
-            .buildAsyncClient();
+    // <LightAppExample>
+    OpenAIAsyncClient client = new OpenAIClientBuilder()
+        .credential(new AzureKeyCredential(AZURE_CLIENT_KEY))
+        .endpoint(CLIENT_ENDPOINT)
+        .buildAsyncClient();
 
-        // Create your AI service client
-        ChatCompletionService chatCompletionService = OpenAIChatCompletion.builder()
-            .withModelId(MODEL_ID)
-            .withOpenAIAsyncClient(client)
-            .build();
+    // <importplugin>
+    // Import the LightsPlugin
+    KernelPlugin lightPlugin = KernelPluginFactory.createFromObject(new LightsPlugin(),
+        "LightsPlugin");
+    // </importplugin>
 
-        // Import the LightsPlugin
-        KernelPlugin lightPlugin = KernelPluginFactory.createFromObject(new LightsPlugin(),
-            "LightsPlugin");
+    // <createservice>
+    // Create your AI service client
+    ChatCompletionService chatCompletionService = OpenAIChatCompletion.builder()
+        .withModelId(MODEL_ID)
+        .withOpenAIAsyncClient(client)
+        .build();
 
-        // Create a kernel with Azure OpenAI chat completion and plugin
-        Kernel kernel = Kernel.builder()
-            .withAIService(ChatCompletionService.class, chatCompletionService)
-            .withPlugin(lightPlugin)
-            .build();
+    // <buildkernel>
+    // Create a kernel with Azure OpenAI chat completion and plugin
+    Kernel kernel = Kernel.builder()
+        .withAIService(ChatCompletionService.class, chatCompletionService)
+        .withPlugin(lightPlugin)
+        .build();
+    // </buildkernel>
+    // </createservice>
 
-        // Add a converter to the kernel to show it how to serialise LightModel objects into a prompt
-        ContextVariableTypes
-            .addGlobalConverter(
-                ContextVariableTypeConverter.builder(LightModel.class)
-                    .toPromptString(new Gson()::toJson)
-                    .build());
+    // Add a converter to the kernel to show it how to serialise LightModel objects into a prompt
+    ContextVariableTypes
+        .addGlobalConverter(
+            ContextVariableTypeConverter.builder(LightModel.class)
+                .toPromptString(new Gson()::toJson)
+                .build());
 
-        // Enable planning
-        InvocationContext invocationContext = new InvocationContext.Builder()
-            .withReturnMode(InvocationReturnMode.LAST_MESSAGE_ONLY)
-            .withToolCallBehavior(ToolCallBehavior.allowAllKernelFunctions(true))
-            .build();
+    // <enableplanning>
+    // Enable planning
+    InvocationContext invocationContext = new InvocationContext.Builder()
+        .withReturnMode(InvocationReturnMode.LAST_MESSAGE_ONLY)
+        .withToolCallBehavior(ToolCallBehavior.allowAllKernelFunctions(true))
+        .build();
+    // </enableplanning>
 
-        // Create a history to store the conversation
-        ChatHistory history = new ChatHistory();
+    // Create a history to store the conversation
+    ChatHistory history = new ChatHistory();
 
-        // Initiate a back-and-forth chat
-        Scanner scanner = new Scanner(System.in);
-        String userInput;
-        do {
-            // Collect user input
-            System.out.print("User > ");
-            userInput = scanner.nextLine();
-            // Add user input
-            history.addUserMessage(userInput);
+    // Initiate a back-and-forth chat
+    Scanner scanner = new Scanner(System.in);
+    String userInput;
+    do {
+      // Collect user input
+      System.out.print("User > ");
 
-            // Prompt AI for response to users input
-            List<ChatMessageContent<?>> results = chatCompletionService
-                .getChatMessageContentsAsync(history, kernel, invocationContext)
-                .block();
+      // <prompt>
+      userInput = scanner.nextLine();
+      // Add user input
+      history.addUserMessage(userInput);
 
-            for (ChatMessageContent<?> result : results) {
-                // Print the results
-                if (result.getAuthorRole() == AuthorRole.ASSISTANT && result.getContent() != null) {
-                    System.out.println("Assistant > " + result);
-                }
-                // Add the message from the agent to the chat history
-                history.addMessage(result);
-            }
-        } while (userInput != null && !userInput.isEmpty());
+      // Prompt AI for response to users input
+      List<ChatMessageContent<?>> results = chatCompletionService
+          .getChatMessageContentsAsync(history, kernel, invocationContext)
+          .block();
+      // </prompt>
 
-        // </LightAppExample>
-    }
+      for (ChatMessageContent<?> result : results) {
+        // Print the results
+        if (result.getAuthorRole() == AuthorRole.ASSISTANT && result.getContent() != null) {
+          System.out.println("Assistant > " + result);
+        }
+        // Add the message from the agent to the chat history
+        history.addMessage(result);
+      }
+    } while (userInput != null && !userInput.isEmpty());
+
+    // </LightAppExample>
+  }
 }
--- samples/java/learnDocs/LightsApp/src/main/java/LightsPlugin.java
+++ samples/java/learnDocs/LightsApp/src/main/java/LightsPlugin.java
@@ -3,40 +3,40 @@
 import com.microsoft.semantickernel.semanticfunctions.annotations.DefineKernelFunction;
 import com.microsoft.semantickernel.semanticfunctions.annotations.KernelFunctionParameter;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-import java.util.Optional;
+import java.util.Map;
 
+// <plugin>
 public class LightsPlugin {
 
-    // Mock data for the lights
-    private final List<LightModel> lights = new ArrayList<>();
+  // Mock data for the lights
+  private final Map<Integer, LightModel> lights = new HashMap<>();
 
-    public LightsPlugin() {
-        lights.add(new LightModel(1, "Table Lamp", false));
-        lights.add(new LightModel(2, "Porch light", false));
-        lights.add(new LightModel(3, "Chandelier", true));
+  public LightsPlugin() {
+    lights.put(1, new LightModel(1, "Table Lamp", false));
+    lights.put(2, new LightModel(2, "Porch light", false));
+    lights.put(3, new LightModel(3, "Chandelier", true));
+  }
+
+  @DefineKernelFunction(name = "get_lights", description = "Gets a list of lights and their current state")
+  public List<LightModel> getLights() {
+    System.out.println("Getting lights");
+    return new ArrayList<>(lights.values());
+  }
+
+  @DefineKernelFunction(name = "change_state", description = "Changes the state of the light")
+  public LightModel changeState(
+      @KernelFunctionParameter(name = "id", description = "The ID of the light to change") int id,
+      @KernelFunctionParameter(name = "isOn", description = "The new state of the light") boolean isOn) {
+    System.out.println("Changing light " + id + " " + isOn);
+    if (!lights.containsKey(id)) {
+      throw new IllegalArgumentException("Light not found");
     }
 
-    @DefineKernelFunction(name = "get_lights", description = "Gets a list of lights and their current state")
-    public List<LightModel> getLights() {
-        System.out.println("Getting lights");
-        return lights;
-    }
+    lights.get(id).setIsOn(isOn);
 
-    @DefineKernelFunction(name = "change_state", description = "Changes the state of the light")
-    public LightModel changeState(
-        @KernelFunctionParameter(name = "id", description = "The ID of the light to change") int id,
-        @KernelFunctionParameter(name = "isOn", description = "The new state of the light") boolean isOn) {
-        System.out.println("Changing light " + id + " " + isOn);
-        Optional<LightModel> light = lights.stream()
-            .filter(l -> l.getId() == id)
-            .findFirst();
-
-        if (light.isEmpty()) {
-            throw new IllegalArgumentException("Light not found");
-        }
-        light.get().setIsOn(isOn);
-
-        return light.get();
-    }
+    return lights.get(id);
+  }
 }
+// </plugin>
